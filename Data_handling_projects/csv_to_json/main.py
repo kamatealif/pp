a9c30f5bb43e71fcec3301f7fd4b1fd5fefaa985
kamatealif <kamatealip1@gmail.com>
2025-08-15 11:53:27 +0530

    Add data preview functionality to display a limited number of records

--- Data_handling_projects/csv_to_json/main.py
+++ Data_handling_projects/csv_to_json/main.py
@@ -19,6 +19,15 @@ def convert_to_json(output_file, data):
         json.dump(data, file, indent=4, ensure_ascii=False)
     print(f"{len(data)} objects saved to {output_file}")
     
+def show_data(data, preview_count = 5):
+    if not data:
+        print("No data to display.")
+    else:
+
+        print(f"Displaying first {preview_count} records:")
+        for i, record in enumerate(data[:preview_count]):
+            print(f"Record {1 + 1} : {record}")
+    
 
 if __name__ == "__main__":
     FILE_NAME = 'data.csv'
@@ -29,6 +38,17 @@ if __name__ == "__main__":
         data = load_csv(FILE_NAME)
 
         convert_to_json(OUTPUT_FILE, data)
+
+        choice = input("Do you want to preview the data? (yes/no): ").strip().lower()
+        if choice == 'yes' or choice == 'y':
+            preview_count = int(input("How many records would you like to preview? "))
+            show_data(data, preview_count)
+        else:
+            print("Preview skipped.")
+    except FileNotFoundError as fnf_error:
+        print(fnf_error)
+    except ValueError as ve:
+        print(ve)
 
     except Exception as e:
         print(f"An error occurred: {e}")
